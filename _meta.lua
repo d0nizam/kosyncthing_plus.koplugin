1631086d0bd41866b903fbbe52f7c92a92e03885
d0nizam <13771489+d0nizam@users.noreply.github.com>
2026-06-18 15:52:28 +0300

    Remove name field from _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -6,7 +6,6 @@
 local _ = require("syncthing_i18n").gettext
 
 return {
-    name        = "kosyncthing_plus",
     fullname    = _("KOSyncthing+"),
     description = _([[Continuously sync files with other devices in a peer-to-peer manner.]]),
     version     = "v1.1.8",
